1ff1a60812f41ff5c5d91fb266da814cd92a2851
Arseniy Fomin <fomin@cs.petrsu.ru>
2020-03-04 11:22:22 +0300

    blyad

--- tests/square_test.h
+++ tests/square_test.h
@@ -2,13 +2,29 @@
 #define SQUARE_TEST_H
 
 #include <gtest/gtest.h>
+#include <io.h>
+#include <cstdio>
 
 extern "C" {
 #include "square.h"
 }
 
 TEST(squareTest, positive) {
-    ;
+    /* Настройка чтения выходного потока */
+    int outFd = open(testOutputFile, O_RDWR | O_CREAT );
+    int oldOutput = dup(stdout);
+    dup2(outFd, stdout);
+
+    /* Запуск функции */
+    ASSERT_EQ(square(1, 5, 6), 2);
+    /* Проверка корней */
+    double x1, x2;
+    fscanf(testOutputFile, "%lf %lf", &x1, &x2);
+    ASSERT_EQ(x1, -2.0);
+    ASSERT_EQ(x2, -3.0);
+
+    /* Закрытие файла */
+
 }
 
 TEST(squareTest, eqzero) {
