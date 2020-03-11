e3198e2ab69a12dc372e5fe0c207c3879da9c91d
Arsenii Fomin <fomin@cs.petrsu.ru>
2020-03-11 10:48:23 +0300

    fifi

--- tests/square_test.h
+++ tests/square_test.h
@@ -3,6 +3,7 @@
 
 #include <gtest/gtest.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -14,26 +15,30 @@ extern "C" {
 }
 
 TEST(squareTest, positive) {
+
     /* Настройка чтения выходного потока */
-    int outFd = open("temp/testOutputFile", O_WRONLY | O_CREAT );
+    int outFd = open("temp/test1", O_WRONLY | O_CREAT);
     int oldOutput = dup(1);
     dup2(outFd, 1);
 
     /* Запуск функции */
     ASSERT_EQ(square(1, 5, 6), 2);
-    /* Считывание корней */
-    double x1, x2;
-    char *buf = (char *)malloc(sizeof(char) * 512);
-    read(outFd, buf, 512);
-    sscanf(buf, "%lf %lf", &x1, &x2);
-    free(buf);
-    /* Проверка корней */
-    ASSERT_DOUBLE_EQ(x1, -2.0);
-    ASSERT_DOUBLE_EQ(x2, -3.0);
 
     /* Закрытие файла */
     close(outFd);
     dup2(oldOutput, 1);
+
+    /* Считывание корней */
+    double x1, x2;
+    FILE *f;
+    if ((f = fopen("temp/test1", "r")) == NULL)
+        FAIL();
+    fscanf(f, "%lf %lf", &x1, &x2);
+    fclose(f);
+
+    /* Проверка корней */
+    ASSERT_DOUBLE_EQ(x1, -2.0);
+    ASSERT_DOUBLE_EQ(x2, -3.0);
 }
 
 #endif // SQUARE_TEST_H
